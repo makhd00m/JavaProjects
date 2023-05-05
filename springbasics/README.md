9b5cac1383cb634ab9b889903da608acdf3e24b4
makhd00m <smhayat225@outlook.com>
2023-05-06 00:15:20 +0530

    spring basics : basic task manager API with add task and fetch task functionality
    Signed-off-by: makhd00m <smhayat225@outlook.com>

---
+++ springbasics/README.md
@@ -0,0 +1,7 @@
+# Spring Boot Basic Project
+
+## Development
+
+### Test API
+
+There  is [a postman collection](./TaskManagerSpring.postman_collection.json) to test the API
--- springbasics/src/main/java/com/scaler/springbasics/hello/HelloController.java
+++ springbasics/src/main/java/com/scaler/springbasics/hello/HelloController.java
@@ -1,9 +1,6 @@
-package com.scaler.springbasics;
+package com.scaler.springbasics.hello;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello")
